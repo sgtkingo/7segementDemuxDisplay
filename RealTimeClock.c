5f0436f2f51cdb2a65f24da665f11c1587422a7a
sgtkingo <33989823+sgtkingo@users.noreply.github.com>
2019-10-25 17:42:54 +0200

    upd done 251019

--- RealTimeClock.c
+++ RealTimeClock.c
@@ -1,72 +1,7 @@
 //Author: Jiri Konecny 
 // PIC18LF46K22 Configuration Bit Settings
 
-// 'C' source line config statements
-
-// CONFIG1H
-#pragma config FOSC = INTIO67   // Oscillator Selection bits (Internal oscillator block)
-#pragma config PLLCFG = OFF     // 4X PLL Enable (Oscillator used directly)
-#pragma config PRICLKEN = ON    // Primary clock enable bit (Primary clock is always enabled)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
-#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
-
-// CONFIG2L
-#pragma config PWRTEN = OFF     // Power-up Timer Enable bit (Power up timer disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable bits (Brown-out Reset enabled and controlled by software (SBOREN is enabled))
-#pragma config BORV = 190       // Brown Out Reset Voltage bits (VBOR set to 1.90 V nominal)
-
-// CONFIG2H
-#pragma config WDTEN = ON       // Watchdog Timer Enable bits (WDT is always enabled. SWDTEN bit has no effect)
-#pragma config WDTPS = 16384    // Watchdog Timer Postscale Select bits (1:16384)
-
-// CONFIG3H
-#pragma config CCP2MX = PORTC1  // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<5:0> pins are configured as analog input channels on Reset)
-#pragma config CCP3MX = PORTB5  // P3A/CCP3 Mux bit (P3A/CCP3 input/output is multiplexed with RB5)
-#pragma config HFOFST = ON      // HFINTOSC Fast Start-up (HFINTOSC output and ready status are not delayed by the oscillator stable status)
-#pragma config T3CMX = PORTC0   // Timer3 Clock input mux bit (T3CKI is on RC0)
-#pragma config P2BMX = PORTD2   // ECCP2 B output mux bit (P2B is on RD2)
-#pragma config MCLRE = EXTMCLR  // MCLR Pin Enable bit (MCLR pin enabled, RE3 input pin disabled)
-
-// CONFIG4L
-#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config LVP = ON         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled if MCLRE is also 1)
-#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
-
-// CONFIG5L
-#pragma config CP0 = OFF        // Code Protection Block 0 (Block 0 (000800-003FFFh) not code-protected)
-#pragma config CP1 = OFF        // Code Protection Block 1 (Block 1 (004000-007FFFh) not code-protected)
-#pragma config CP2 = OFF        // Code Protection Block 2 (Block 2 (008000-00BFFFh) not code-protected)
-#pragma config CP3 = OFF        // Code Protection Block 3 (Block 3 (00C000-00FFFFh) not code-protected)
-
-// CONFIG5H
-#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
-#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
-
-// CONFIG6L
-#pragma config WRT0 = OFF       // Write Protection Block 0 (Block 0 (000800-003FFFh) not write-protected)
-#pragma config WRT1 = OFF       // Write Protection Block 1 (Block 1 (004000-007FFFh) not write-protected)
-#pragma config WRT2 = OFF       // Write Protection Block 2 (Block 2 (008000-00BFFFh) not write-protected)
-#pragma config WRT3 = OFF       // Write Protection Block 3 (Block 3 (00C000-00FFFFh) not write-protected)
-
-// CONFIG6H
-#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
-#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block (000000-0007FFh) not write-protected)
-#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
-
-// CONFIG7L
-#pragma config EBTR0 = OFF      // Table Read Protection Block 0 (Block 0 (000800-003FFFh) not protected from table reads executed in other blocks)
-#pragma config EBTR1 = OFF      // Table Read Protection Block 1 (Block 1 (004000-007FFFh) not protected from table reads executed in other blocks)
-#pragma config EBTR2 = OFF      // Table Read Protection Block 2 (Block 2 (008000-00BFFFh) not protected from table reads executed in other blocks)
-#pragma config EBTR3 = OFF      // Table Read Protection Block 3 (Block 3 (00C000-00FFFFh) not protected from table reads executed in other blocks)
-
-// CONFIG7H
-#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot Block (000000-0007FFh) not protected from table reads executed in other blocks)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
-#include <xc.h>
+#include "PICF18LF46K22_ConfigSFR.h"
 
 #define DMX_A PORTEbits.RE0
 #define DMX_B PORTEbits.RE1
@@ -82,28 +17,25 @@
 #define numbersDataSize 10
 #define testDataSize 8
 
-#define true 1
-#define false 0
-
-#define _XTAL_FREQ 16000000
-
-char demuxSelect[]={0b00000000, 0b00000001, 0b00000010, 0b00000011};
+const char demuxSelect[]={0b00000000, 0b00000001, 0b00000010, 0b00000011};
 char demuxData[]={0xFF, 0xFF, 0xFF, 0xFF};
 
-char numbersData[]={ 0b11000000 /*0*/ ,0b11111001 /*1*/, 0b10100100  /*2*/,0b10110000 /*3*/,0b10011001 /*4*/,0b10010010 /*5*/,0b10000010 /*6*/,0b11111000 /*7*/,0b10000000 /*8*/,0b10010000 /*9*/};
+const char numbersData[]={ 0b11000000 /*0*/ ,0b11111001 /*1*/, 0b10100100  /*2*/,0b10110000 /*3*/,0b10011001 /*4*/,0b10010010 /*5*/,0b10000010 /*6*/,0b11111000 /*7*/,0b10000000 /*8*/,0b10010000 /*9*/};
 int numbersValue[]={0,0,0,0};
 
-char charsetData[]={0b11111101 /*-*/};
+const char charsetData[]={0b11111101 /*-*/};
 
-char selectDot[]={0b11111111, 0b01111111};
+const char selectDot[]={0b11111111, 0b01111111};
 char dotSelector[]={false,true,false,false};
 
+bit mode;
 int indexer=0;
-unsigned int seconds=19,mins=55,hours=10,counter_ms=0;
+unsigned int seconds=0,mins=0,hours=0,counter_ms=0;
 
-void Init(){
+void InitDevice(){
     OSCCON=0b01111100; //osc setting, 16 MHz, internal by FOSCH
     INTCON=0b11100000; //int setting, global,pir, timer0
+    
     PIE5bits.TMR4IE=1; //timer4 int owerflow enable
     
     INTCON2bits.NOT_RBPU=1; //potrB pullup enable
@@ -136,6 +68,7 @@ void ClearDevice(){
     PORTD=1;
     DMX_A=DMX_B=0;
     indexer=0;
+    mode=true;
 }
 
 void TestDevice(){
@@ -169,21 +102,45 @@ void parseValues(int F, int S){
 }
 
 void CheckButtons(){
-    if((!BTN_A_P) && (mins<60)){
-        __delay_ms(10);
-        if(!BTN_A_P)mins++;
+    if (BTN_A_P&&BTN_A_N&&BTN_B_P&&BTN_B_N)return;
+    if (!(BTN_A_P||BTN_A_N||BTN_B_P||BTN_B_N)){
+            mode=!mode;
+            __delay_ms(500);
     }
-    if((!BTN_A_N) && (mins>0)){
-        __delay_ms(10);
-        if(!BTN_A_N)mins--;
-    }
-    if((!BTN_B_P) && (hours<24)){
-        __delay_ms(10);
-        if(!BTN_B_P)hours++;
-    }
-    if((!BTN_B_N) && (hours>0)){
-        __delay_ms(10);
-        if(!BTN_B_N)hours--;
+    if(!mode){
+        if((!BTN_A_P) && (mins<60)){
+            asm("NOP");
+            if(!BTN_A_P)mins++;
+        }
+        if((!BTN_A_N) && (mins>0)){
+            asm("NOP");
+            if(!BTN_A_N)mins--;
+        }
+        if((!BTN_B_P) && (hours<24)){
+            asm("NOP");
+            if(!BTN_B_P)hours++;
+        }
+        if((!BTN_B_N) && (hours>0)){
+            asm("NOP");
+            if(!BTN_B_N)hours--;
+        }
+    }else {
+        if((!BTN_A_P) && (seconds<60)){
+            asm("NOP");
+            if(!BTN_A_P)seconds++;
+        }
+        if((!BTN_A_N) && (seconds>0)){
+            asm("NOP");
+            if(!BTN_A_N)seconds--;
+        }
+        if((!BTN_B_P) && (mins<60)){
+            asm("NOP");
+            if(!BTN_B_P)mins++;
+        }
+        if((!BTN_B_N) && (mins>0)){
+            asm("NOP");
+            if(!BTN_B_N)mins--;
+        }    
     }
 }
 
@@ -213,12 +170,13 @@ void timeStep(){
             demuxData[i]=numbersData[numbersValue[i]];
             demuxData[i]&=selectDot[dotSelector[i]];
         }  
-        parseValues(mins,seconds);
+        if(mode)parseValues(mins,seconds);
+        else parseValues(hours,mins);
 }
 
 
 void main(void) {
-    Init();
+    InitDevice();
     ClearDevice();
     TestDevice();
     T0CONbits.TMR0ON=1;
@@ -227,8 +185,8 @@ void main(void) {
     while(1){
         asm("NOP");
         asm("CLRWDT");
-        //CheckButtons();
-        __delay_ms(250);        
+        CheckButtons();
+        __delay_ms(50);        
     }
 }
 
